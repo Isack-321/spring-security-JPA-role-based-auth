a61d8d921da7a0171d461dd20df1824b49868e28
Isack <ituwei21@gmail.com>
2022-02-09 11:07:53 +0300

    getting started

---
+++ src/main/java/com/ituwei/roleBaesdAuth/RoleBaesdAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ituwei.roleBaesdAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBaesdAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBaesdAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ituwei/roleBaesdAuth/RoleBaesdAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ituwei.roleBaesdAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBaesdAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
